220e7010fe6e1d35b3ccb54b7c5ce514b034cac6
squidbus <175574877+squidbus@users.noreply.github.com>
2026-05-20 14:26:41 -0700

    Fix MARATHON_RECOMP define in compile_shader

--- MarathonRecomp/gpu/shader/hlsl/conditional_survey_ps.hlsl
+++ MarathonRecomp/gpu/shader/hlsl/conditional_survey_ps.hlsl
@@ -1,5 +1,14 @@
 #include "../../../../tools/XenosRecomp/XenosRecomp/shader_common.h"
 
+#ifndef __spirv__
+
+cbuffer SharedConstants : register(b2, space4)
+{
+	DEFINE_SHARED_CONSTANTS();
+};
+
+#endif
+
 [earlydepthstencil]
 float4 shaderMain() : SV_Target
 {
